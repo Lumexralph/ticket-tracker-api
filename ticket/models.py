8665bd4a26d9de8d2284573aedb048e4f924a4f1
Olumide Ogundele <olumideralph@gmail.com>
2020-03-10 15:28:48 +0100

    create ticket model and migrations

--- ticket/models.py
+++ ticket/models.py
@@ -1,3 +1,43 @@
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 from django.db import models
 
-# Create your models here.
+from shared.models.base_model import BaseModel
+from account.models import User
+
+
+class Ticket(BaseModel):
+    """Model for a ticket created by a user"""
+    ENHANCEMENT = 'EN'
+    BUGFIX = "BF"
+    DEVELOPMENT = "DV"
+    QUALITY_ASSURANCE = "QA"
+
+    TICKET_TYPES = [
+        (ENHANCEMENT, 'enhancement'),
+        (BUGFIX, 'bugfix'),
+        (DEVELOPMENT, 'development'),
+        (QUALITY_ASSURANCE, 'quality assurance')
+    ]
+
+    class Meta:
+        """Class Meta to add more information for the ticket model"""
+        ordering = ('type',)
+
+    creator = models.ForeignKey(User, related_name='tickets', on_delete=models.CASCADE)
+    assigned_to = models.ForeignKey(
+        User,
+        related_name='assigned_tickets',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    activated = models.BooleanField(default=False)
+    summary = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    type = models.CharField(
+        max_length=2,
+        choices=TICKET_TYPES,
+        default=ENHANCEMENT,
+    )
+    complexity = models.IntegerField(validators=[MinLengthValidator(0), MaxLengthValidator(5)])
+    estimated = models.IntegerField(validators=[MinLengthValidator(0)])
+
